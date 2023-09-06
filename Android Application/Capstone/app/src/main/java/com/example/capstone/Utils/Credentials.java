4bb347ec281c3919ad155a666dd06eaa57eeae0d
Sanjay_Kendole <87925123+sanjay2202@users.noreply.github.com>
2023-09-06 21:42:49 +0530

    Update Credentials.java

--- Android Application/Capstone/app/src/main/java/com/example/capstone/Utils/Credentials.java
+++ Android Application/Capstone/app/src/main/java/com/example/capstone/Utils/Credentials.java
@@ -2,7 +2,7 @@ package com.example.capstone.Utils;
 
 public class Credentials {
 
-    public static final String BASE_URL = "http://192.168.136.1:5000";
+    public static final String BASE_URL = "YourIpAddress";
 
     public static boolean RANDOM = false;
 
